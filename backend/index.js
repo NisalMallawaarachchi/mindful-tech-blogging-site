56dc1217b8164a1e815d660b5da9e0990f47a7fa
ShakyaGurusinghe <shakyapersonal1@gmail.com>
2024-12-31 18:00:03 +0530

    Mongodb is connected!

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
+const dbConnection = require("./config/db");
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+dbConnection();
 app.get('/',(req,res)=>res.send("Welcome to Mindful Tech Blogging!"));
 
 app.listen(PORT,()=>{
